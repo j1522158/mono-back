1f828e6740ebb7552dc0085ace9c328639afee27
j1522158 <j1522158vu@gmail.com>
2025-02-02 02:44:50 +0900

    [Fix]ID追加

--- src/main/java/jp/co/monocrea/entity/User.java
+++ src/main/java/jp/co/monocrea/entity/User.java
@@ -1,11 +1,16 @@
 package jp.co.monocrea.entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User extends PanacheEntity {
+
+    @Id
+    @GeneratedValue
+    public Long id;
+
     @Column(nullable = false)
     public String name;
 }
---
+++ src/main/resources/db/migration/V1__insert_users.sql
@@ -0,0 +1,13 @@
+INSERT INTO users (id, name) VALUES (1, UserA);
+INSERT INTO users (id, name) VALUES (2, UserB);
+INSERT INTO users (id, name) VALUES (3, UserC);
+INSERT INTO users (id, name) VALUES (4, UserD);
+INSERT INTO users (id, name) VALUES (5, UserE);
+INSERT INTO users (id, name) VALUES (6, UserF);
+INSERT INTO users (id, name) VALUES (7, UserG);
+INSERT INTO users (id, name) VALUES (8, UserH);
+INSERT INTO users (id, name) VALUES (9, UserI);
+INSERT INTO users (id, name) VALUES (10, UserJ);
+INSERT INTO users (id, name) VALUES (11, UserK);
+INSERT INTO users (id, name) VALUES (12, UserL);
+
