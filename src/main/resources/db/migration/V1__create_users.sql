3529c30ddb81eca6ccb61c7a3b4f6441be9d584b
j1522158 <j1522158vu@gmail.com>
2025-02-02 18:18:58 +0900

    [Fix]sql型修正

--- src/main/resources/db/migration/V1__create_users.sql
+++ src/main/resources/db/migration/V1__create_users.sql
@@ -1,23 +1,23 @@
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
 
-INSERT INTO users (name) VALUES ('UserA');
-INSERT INTO users (name) VALUES ('UserB');
-INSERT INTO users (name) VALUES ('UserC');
-INSERT INTO users (name) VALUES ('UserD');
-INSERT INTO users (name) VALUES ('UserE');
-INSERT INTO users (name) VALUES ('UserF');
-INSERT INTO users (name) VALUES ('UserG');
-INSERT INTO users (name) VALUES ('UserH');
-INSERT INTO users (name) VALUES ('UserI');
-INSERT INTO users (name) VALUES ('UserJ');
-INSERT INTO users (name) VALUES ('UserK');
-INSERT INTO users (name) VALUES ('UserL');
-INSERT INTO users (name) VALUES ('UserM');
-INSERT INTO users (name) VALUES ('UserN');
-INSERT INTO users (name) VALUES ('UserO');
-INSERT INTO users (name) VALUES ('UserP');
+INSERT INTO users (id, name) VALUES (1, 'UserA');
+INSERT INTO users (id, name) VALUES (2, 'UserB');
+INSERT INTO users (id, name) VALUES (3, 'UserC');
+INSERT INTO users (id, name) VALUES (4, 'UserD');
+INSERT INTO users (id, name) VALUES (5, 'UserE');
+INSERT INTO users (id, name) VALUES (6, 'UserF');
+INSERT INTO users (id, name) VALUES (7, 'UserG');
+INSERT INTO users (id, name) VALUES (8, 'UserH');
+INSERT INTO users (id, name) VALUES (9, 'UserI');
+INSERT INTO users (id, name) VALUES (10, 'UserJ');
+INSERT INTO users (id, name) VALUES (11, 'UserK');
+INSERT INTO users (id, name) VALUES (12, 'UserL');
+INSERT INTO users (id, name) VALUES (13, 'UserM');
+INSERT INTO users (id, name) VALUES (14, 'UserN');
+INSERT INTO users (id, name) VALUES (15, 'UserO');
+INSERT INTO users (id, name) VALUES (16, 'UserP');
 
 
